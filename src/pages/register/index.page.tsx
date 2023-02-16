2a790d0358c75c30d5d8cfe9f816e2d12a324b69
Juliana Daroz <julianadaroz@gmail.com>
2023-02-16 10:50:19 -0300

    feat: add Prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
@@ -26,10 +28,19 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
     console.log(data)
