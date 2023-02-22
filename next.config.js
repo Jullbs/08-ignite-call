7977d602e40f19799985edb1f9d9ae55bae7da7a
Juliana Daroz <julianadaroz@gmail.com>
2023-02-22 11:08:47 -0300

    feat: add update-profile page and add profile api route

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   pageExtensions: [
     'page.tsx',
     'api.ts',
     'api.tsx',
   ],
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 }
 
 module.exports = nextConfig
