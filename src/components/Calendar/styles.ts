a639ad0c3740e971a57d56f69e57327d2933f3f9
Juliana Daroz <julianadaroz@gmail.com>
2023-02-28 11:27:30 -0300

    feat: adjusting calendar display of days and months

--- src/components/Calendar/styles.ts
+++ src/components/Calendar/styles.ts
@@ -15,6 +15,7 @@ export const CalendarHeader = styled('div', {
 
 export const CalendarTitle = styled(Text, {
   fontWeight: '$medium',
+  textTransform: 'capitalize',
 
   span: {
     color: '$gray200',
@@ -81,7 +82,7 @@ export const CalendarDay = styled('button', {
   borderRadius: '$sm',
 
   '&:disabled': {
-    backgorund: 'none',
+    background: 'none',
     cursor: 'default',
     opacity: 0.4,
   },
